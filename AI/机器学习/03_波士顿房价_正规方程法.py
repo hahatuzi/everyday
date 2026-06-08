088b6c229e352d84927f9ca59d3dd121b4ad155e
hahatuzi <>
2026-06-08 23:05:36 +0800

    feat:线性回归

--- AI/机器学习/03_波士顿房价_正规方程法.py
+++ AI/机器学习/03_波士顿房价_正规方程法.py
@@ -7,5 +7,13 @@ from sklearn.linear_model import Ridge,RidgeCV
 
 import pandas as pd
 import numpy as np
-
+# 1.加载波士顿房价数据
 data_url = 'http://lib.stat.cmu.edu/datasets/boston'
+raw_df = pd.read_csv(data_url, sep='\\s+',skiprows=2,header=None)
+data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+target = raw_df.values[1::2, 2]
+
+print(f'特征：{data.shape}')
+print(f'特征：{target.shape}')
+print(f'特征数据：{data[:5]}')
+print(f'特征数据：{target[:5]}')
---
+++ AI/机器学习/03_线性回归_欠拟合.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import plot_tree
+
+# 欠拟合
+def under_fitting():
+    np.random.seed(23)
+    x = np.random.uniform(-3, 3, size=100)
+    y = 0.5 * x ** 2 + x + 2 + np.random.normal(0,1,size=100)
+    print(f'特征(x):{x}')
+    print(f'特征(y):{y}')
+
+    X = x.reshape(-1,1)
+    print(f'处理后的数据为{X[:5]}')
+    estimator = LinearRegression()
+    estimator.fit(X,y)
+    y_predict = estimator.predict(X)
+    print(f'均方误差为{mean_squared_error(y,y_predict)}')
+    plt.scatter(X,y)
+    plt.plot(X,y_predict,color='red')
+    plt.show()
+
+
+# 正好拟合
+def just_fitting():
+    np.random.seed(23)
+    x = np.random.uniform(-3, 3, size=100)
+    y = 0.5 * x ** 2 + x + 2 + np.random.normal(0, 1, size=100)
+    print(f'特征(x):{x}')
+    print(f'特征(y):{y}')
+
+    X = x.reshape(-1,1)
+    X2 = np.hstack([X , X ** 2])
+    print(f'处理后的数据为{X2[:5]}')
+
+    estimator = LinearRegression()
+    estimator.fit(X2, y)
+    y_predict = estimator.predict(X2)
+    print(f'均方误差为{mean_squared_error(y, y_predict)}')
+    plt.scatter(x, y)
+    plt.plot(np.sort(x), y_predict[np.argsort(x)], color='red')
+    plt.show()
+
+# 过拟合
+def over_fitting():
+    np.random.seed(23)
+    x = np.random.uniform(-3, 3, size=100)
+    y = 0.5 * x ** 2 + x + 2 + np.random.normal(0, 1, size=100)
+    print(f'特征(x):{x}')
+    print(f'特征(y):{y}')
+
+    X = x.reshape(-1,1)
+    X3 = np.hstack([X , X ** 2, X ** 3, X ** 4, X ** 5, X ** 6, X ** 7, X ** 8, X ** 9])
+    print(f'处理后的数据为{X3[:5]}')
+
+    estimator = LinearRegression()
+    estimator.fit(X3, y)
+    y_predict = estimator.predict(X3)
+    print(f'均方误差为{mean_squared_error(y, y_predict)}')
+    plt.scatter(x, y)
+    plt.plot(np.sort(x), y_predict[np.argsort(x)], color='red')
+    plt.show()
+
+if __name__ == '__main__':
+    # under_fitting()
+    # just_fitting()
+    over_fitting()
